23d8c5788f92aa8da013fbcc10e8f4651461ca97
thomas-guehenneux <97498481+thomas-guehenneux@users.noreply.github.com>
2022-12-04 20:04:42 +0900

    test

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'VitePress',
+  description: 'Just playing around.',
+  themeConfig: {
+    nav: [
+      { text: 'Index', link: '/index' },
+      { text: 'Page1', link: '/page1' },
+      { text: 'Page2', link: '/page2' },
+      { text: 'Changelog', link: 'https://github.com/' }
+    ]
+  }
+}
